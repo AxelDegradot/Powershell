ce3cc70c5d1ec23f4f1f86bc698efaaf4257c9c4
AxelDegradot <axel.degradot@cciformation72.fr>
2025-03-02 21:26:00 +0100

    Add files via upload

---
+++ Test.ps1
@@ -0,0 +1,13 @@
+﻿$remoteComputer = "DESKTOP-0A2V37H"  # Nom ou adresse IP pour le pour le pc 
+$remotePath = "\\DESKTOP-0A2V37H\c`$\Users\t.kilo\Desktop\test\Test.txt"  # Chemin réseau du fichier
+
+if (Test-Path $remotePath) {
+    $lastModified = (Get-Item $remotePath).LastWriteTime
+    $name = (Get-Item $remotePath).Name
+
+    Add-Type -AssemblyName PresentationFramework
+
+    [System.Windows.MessageBox]::Show("Le fichier $name sur $remoteComputer a été modifié pour la dernière fois le : $lastModified", "Information", "OK", "Information")
+} else {
+    [System.Windows.MessageBox]::Show("Le fichier spécifié n'existe pas sur $remoteComputer. Vérifiez le chemin et l'accessibilité.", "Erreur", "OK", "Error")
+}
